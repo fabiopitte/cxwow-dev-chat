76837510896656561fd9951be3543e46ffe2a49b
Fabio Pitte <fabio.pitte@emanagecrm.com>
2023-01-31 15:58:20 -0300

    fix

---
+++ chat.js
@@ -0,0 +1,24 @@
+import { io } from 'socket.io-client';
+
+export default function Chat() {
+    var socket = io('ws://localhost:3000')
+
+    var messages = document.getElementById('messages');
+    var form = document.getElementById('form');
+    var input = document.getElementById('input');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        if (input.value) {
+            socket.emit('chat message', input.value);
+            input.value = '';
+        }
+    });
+
+    socket.on('chat message', function (msg) {
+        var item = document.createElement('li');
+        item.textContent = msg;
+        messages.appendChild(item);
+        window.scrollTo(0, document.body.scrollHeight);
+    });
+}
